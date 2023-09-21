68567db5e7e723a68cb1e82dc72e7d2e2e2e9230
Gilbike <marton.dikacz@gmail.com>
2023-09-21 19:31:46 +0200

    feat: :construction: add background image to index page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,25 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Link } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+
+const image = require("../../assets/images/layered-waves-haikei.png");
 
 export default function index() {
   return (
-    <SafeAreaView>
-      <Text>jajajajajajajajajajajajajajaja</Text>
-    </SafeAreaView>
+    // !FIXME: need better resolution image
+    <ImageBackground source={image} style={styles.backgroundImage}>
+      <StatusBar style="light" />
+      <SafeAreaView>
+        <Text>jajajajajajajajajajajajajajaja</Text>
+      </SafeAreaView>
+    </ImageBackground>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+  },
+});
