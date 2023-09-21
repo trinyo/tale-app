feb0e344feffabf1b038286ac5f3d83e325fe564
Gilbike <marton.dikacz@gmail.com>
2023-09-21 19:23:15 +0200

    feat: :lipstick: add navbar icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -21,21 +21,17 @@ export default function _layout() {
         tabBarShowLabel: false,
       }}
     >
-      <Tabs.Screen name="sets" />
-      <Tabs.Screen options={{ tabBarIcon: (props) => <Fogalmamnincs name="home" /> }} name="index" />
-      <Tabs.Screen
-        // Name of the route to hide.
-        name="settings"
-      />
+      <Tabs.Screen options={{ tabBarIcon: (props) => <NavbarIcon name="reorder" focused={props.focused} /> }} name="sets" />
+      <Tabs.Screen options={{ tabBarIcon: (props) => <NavbarIcon name="home" focused={props.focused} /> }} name="index" />
+      <Tabs.Screen options={{ tabBarIcon: (props) => <NavbarIcon name="settings" focused={props.focused} /> }} name="settings" />
     </Tabs>
   );
 }
 
-function Fogalmamnincs(props: { name: keyof typeof MaterialIcons.glyphMap }) {
+function NavbarIcon(props: { name: keyof typeof MaterialIcons.glyphMap; focused: boolean }) {
   return (
-    <View style={{ alignItems: "center" }}>
-      <MaterialIcons name={props.name} size={24} color="black" />
-      <Text>Home</Text>
+    <View style={{ alignItems: "center", backgroundColor: props.focused ? "#e0e0e0" : "transparent", padding: 12, borderRadius: 999 }}>
+      <MaterialIcons name={props.name} size={24} color="#161616" />
     </View>
   );
 }
