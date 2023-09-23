b0e3dece7cb5dc76ee6b39e1aa26f501d20b1a08
Gilbike <marton.dikacz@gmail.com>
2023-09-23 13:48:09 +0200

    refactor: :lipstick: adapt navbar to themes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,8 +2,19 @@ import { View, Text } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
 import { MaterialIcons } from "@expo/vector-icons";
+import { useTheme } from "@/contexts/ThemeProvider";
 
 export default function _layout() {
+  const theme = useTheme();
+
+  const NavbarIcon = (props: { name: keyof typeof MaterialIcons.glyphMap; focused: boolean }) => {
+    return (
+      <View style={{ alignItems: "center", backgroundColor: props.focused ? theme.elevation2.press : "transparent", padding: 12, borderRadius: 999 }}>
+        <MaterialIcons name={props.name} size={24} color={theme.text} />
+      </View>
+    );
+  };
+
   return (
     <Tabs
       screenOptions={{
@@ -15,7 +26,7 @@ export default function _layout() {
           elevation: 0,
           borderRadius: 999,
           height: 50,
-          backgroundColor: "#EAEAEA",
+          backgroundColor: theme.elevation1.hover,
           borderTopWidth: 0,
         },
         tabBarShowLabel: false,
@@ -25,13 +36,5 @@ export default function _layout() {
       <Tabs.Screen options={{ tabBarIcon: (props) => <NavbarIcon name="home" focused={props.focused} /> }} name="index" />
       <Tabs.Screen options={{ tabBarIcon: (props) => <NavbarIcon name="settings" focused={props.focused} /> }} name="settings" />
     </Tabs>
-  );
-}
-
-function NavbarIcon(props: { name: keyof typeof MaterialIcons.glyphMap; focused: boolean }) {
-  return (
-    <View style={{ alignItems: "center", backgroundColor: props.focused ? "#e0e0e0" : "transparent", padding: 12, borderRadius: 999 }}>
-      <MaterialIcons name={props.name} size={24} color="#161616" />
-    </View>
   );
 }
