9795aba327a4591329dcebc4050fcb0eb50806f0
Gilbike <marton.dikacz@gmail.com>
2023-09-23 13:57:06 +0200

    fix: :lipstick: fix missing theme adaptations

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { ImageBackground, StyleSheet } from "react-native";
+import { ImageBackground, StyleSheet, useColorScheme } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import HomeContainer from "../../components/HomeContainer";
@@ -7,8 +7,10 @@ const backgroundLight = require("../../assets/images/bg-light.png");
 const backgroundDark = require("../../assets/images/bg-dark.png");
 
 export default function index() {
+  const colorScheme = useColorScheme() || "light";
+
   return (
-    <ImageBackground source={backgroundDark} style={styles.backgroundImage}>
+    <ImageBackground source={colorScheme === "dark" ? backgroundDark : backgroundLight} style={styles.backgroundImage}>
       <SafeAreaView>
         <HomeContainer />
       </SafeAreaView>
--- app/(tabs)/sets.tsx
+++ app/(tabs)/sets.tsx
@@ -1,13 +1,14 @@
-import { Text, StyleSheet, FlatList, View } from "react-native";
+import { StyleSheet, FlatList, View } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { DataSets } from "../../data/datasets";
-import SetView from "../../components/SetView";
+import { DataSets } from "@/data/datasets";
+import SetView from "@/components/SetView";
+import Label from "@/components/Label";
 
 export default function sets() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Sets</Text>
+      <Label style={styles.title}>Sets</Label>
       <FlatList
         data={DataSets}
         renderItem={({ item }) => <SetView title={item.name} count={item.count} />}
