09be86e74eb8a24072949d6071d81b5ce9a2faa2
Gilbike <marton.dikacz@gmail.com>
2023-09-24 10:17:05 +0200

    refactor: :bento: switch to universal background image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,13 @@
-import { ImageBackground, StyleSheet, useColorScheme } from "react-native";
+import { ImageBackground, StyleSheet } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import HomeContainer from "../../components/HomeContainer";
 
-const backgroundLight = require("../../assets/images/bg-light.png");
-const backgroundDark = require("../../assets/images/bg-dark.png");
+const background = require("../../assets/images/home-bg.png");
 
 export default function index() {
-  const colorScheme = useColorScheme() || "light";
-
   return (
-    <ImageBackground source={colorScheme === "dark" ? backgroundDark : backgroundLight} style={styles.backgroundImage}>
+    <ImageBackground source={background} style={styles.backgroundImage}>
       <SafeAreaView>
         <HomeContainer />
       </SafeAreaView>
