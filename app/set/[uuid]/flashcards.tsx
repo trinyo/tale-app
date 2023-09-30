bccf8105df2c436881ed13c7c172d1cdb534ff0c
Gilbike <marton.dikacz@gmail.com>
2023-09-30 12:00:59 +0200

    feat: :sparkles: add flashcard stepping

--- app/set/[uuid]/flashcards.tsx
+++ app/set/[uuid]/flashcards.tsx
@@ -2,7 +2,7 @@ import { Pressable, StyleSheet, Text, View } from "react-native";
 import React, { useEffect, useMemo, useState } from "react";
 import Label from "@/components/Label";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { MaterialIcons } from "@expo/vector-icons";
+import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
 import { useTheme } from "@/contexts/ThemeProvider";
 import { Colors } from "@/data/Colors";
 import Flashcard from "@/components/Flashcard";
@@ -17,7 +17,7 @@ export default function flashcards() {
   const set = useSet(uuid.toString())?.data;
 
   const [current, setCurrent] = useState(0);
-  const progressPercent = useMemo(() => (current / 75) * 100, [current]);
+  const progressPercent = useMemo(() => (current / (!set ? 100 : Object.keys(set).length)) * 100, [current]);
   const longestDefinition = useMemo(() => {
     if (!set) return 999;
     return Object.values(set)
@@ -37,9 +37,23 @@ export default function flashcards() {
           <MaterialIcons name="subject" size={42} color={theme.text} />
         </Pressable>
       </View>
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center", gap: 8 }}>
         {/* flashcard */}
         <Flashcard data={set} current={current} maximumDifficulity={longestDefinition} />
+        <View style={{ flexDirection: "row", gap: 8 }}>
+          <Pressable
+            onPress={() => setCurrent((value) => (value > 0 ? value - 1 : value))}
+            style={({ pressed }) => [styles.stepButton, { backgroundColor: theme.elevation1[pressed ? "press" : "normal"] }]}
+          >
+            <MaterialCommunityIcons name="arrow-left" size={24} color={theme.text} />
+          </Pressable>
+          <Pressable
+            onPress={() => setCurrent((value) => (value < Object.keys(set).length - 1 ? value + 1 : value))}
+            style={({ pressed }) => [styles.stepButton, { backgroundColor: theme.elevation1[pressed ? "press" : "normal"] }]}
+          >
+            <MaterialCommunityIcons name="arrow-right" size={24} color={theme.text} />
+          </Pressable>
+        </View>
       </View>
       <View style={styles.bottomBar}>
         {/* progress */}
@@ -71,5 +85,9 @@ const styles = StyleSheet.create({
     borderRadius: 999,
     width: "70%",
     height: 20,
+  },
+  stepButton: {
+    borderRadius: 999,
+    padding: 12,
   },
 });
