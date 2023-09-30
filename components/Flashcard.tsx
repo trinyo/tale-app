adbd77037d7c3c3b8a401a05f2f82db2773671cb
Gilbike <marton.dikacz@gmail.com>
2023-09-30 11:34:17 +0200

    fix: :lipstick: fix card clipping issue when rotating

--- components/Flashcard.tsx
+++ components/Flashcard.tsx
@@ -13,6 +13,7 @@ export default function Flashcard({ data, current }: IProps) {
   const theme = useTheme();
 
   const [flipped, setFlipped] = useState(false);
+  const [zFlipped, setZFlipped] = useState(false);
 
   const rotation = useMemo(() => new Animated.Value(0), []);
 
@@ -34,11 +35,17 @@ export default function Flashcard({ data, current }: IProps) {
       easing: Easing.linear,
       useNativeDriver: true,
     }).start();
+
+    setTimeout(() => {
+      setZFlipped(flipped);
+    }, 200);
   }, [flipped]);
 
   return (
     <Pressable onPress={() => setFlipped((value) => !value)} style={{ width: "100%", height: "60%" }}>
-      <Animated.View style={[styles.container, { backgroundColor: theme.elevation1.normal, transform: [{ rotateY: frontRotation }], zIndex: flipped ? 0 : 1 }]}>
+      <Animated.View
+        style={[styles.container, { backgroundColor: theme.elevation1.normal, transform: [{ rotateY: frontRotation }], zIndex: zFlipped ? 0 : 1 }]}
+      >
         <View style={{ height: 20, width: "100%", backgroundColor: Colors.accent.red.normal }} />
         <Label>{Object.keys(data)[current]}</Label>
       </Animated.View>
